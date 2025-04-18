42a04de973f055f6ec2f25f970802543979bb074
João Pedro Lazzaroto Salles <joaopedro.lsalles@gmail.com>
2025-04-17 23:15:00 -0300

    Added a menu to main

--- Main.java
+++ Main.java
@@ -1,35 +1,78 @@
 // Main.java
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         AVLTree tree = new AVLTree();
+        Scanner sc = new Scanner(System.in);
+        int opcao;
 
-        int keys[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1 - Inserir nodo");
+            System.out.println("2 - Deletar nodo");
+            System.out.println("3 - Percorrer arvore");
+            System.out.println("4 - Buscar nodo");
+            System.out.println("5 - Criar arvore teste (1 - 10)");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = sc.nextInt();
 
-        for (int i = 0; i < keys.length; i++) {
-            tree.root = tree.insert(tree.root, keys[i]);
-        }
+            switch (opcao) {
+                case 1:
+                    System.out.print("Informe o valor a inserir: ");
+                    int valInsere = sc.nextInt();
+                    tree.root = tree.insert(tree.root, valInsere);
+                    System.out.println("\n" + "Nodo inserido!" + "\n" + tree.searchNode(tree.root, valInsere));
+                    break;
 
-        System.out.println("Árvore está balanceada antes da exclusão? " + tree.isBalanced(tree.root));
+                case 2:
+                    System.out.print("Informe o valor a deletar: ");
+                    int valDelete = sc.nextInt();
+                    tree.delete(valDelete);
+                    break;
 
-        System.out.println("\n" + tree.searchNode(tree.root, 8));
+                case 3:
+                    System.out.println("Percuso em pre-ordem: ");
+                    tree.preOrder(tree.root);
+                    System.out.println();
+            
+                    System.out.println("Percuso em ordem: ");
+                    tree.inOrder(tree.root);
+                    System.out.println();
+            
+                    System.out.println("Percuso em pos-ordem: ");
+                    tree.postOrder(tree.root);
+                    System.out.println();
+                    break;
 
-        tree.delete(8);
+                case 4:
+                    System.out.print("Informe o valor a buscar: ");
+                    int valBusca = sc.nextInt();
+                    System.out.println("\n " + tree.searchNode(tree.root, valBusca));
+                    break;
 
-        System.out.println("\n" + tree.searchNode(tree.root, 9));
+                case 5:
+                    System.out.print("Inseridos valores de 1 a 10");
+                    int keys[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-        System.out.println("Árvore está balanceada após exclusão de 8? " + tree.isBalanced(tree.root));
+                    for (int i = 0; i < keys.length; i++) {
+                        tree.root = tree.insert(tree.root, keys[i]);
+                    }
+                    break;
 
-        System.out.println("Preorder traversal of constructed AVL tree is : ");
-        tree.preOrder(tree.root);
-        System.out.println();
+                case 0:
+                    System.out.println("Encerrando o programa...");
+                    break;
 
-        System.out.println("Inorder traversal of constructed AVL tree is : ");
-        tree.inOrder(tree.root);
-        System.out.println();
+                default:
+                    break;
+            }
 
-        System.out.println("Postorder traversal of constructed AVL tree is : ");
-        tree.postOrder(tree.root);
-        System.out.println();
+        } while(opcao != 0);
+
+        sc.close();
 
     }
 }
